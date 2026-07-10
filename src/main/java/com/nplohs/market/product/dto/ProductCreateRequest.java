a3b443e107d28cfe8478233d21a06764fec57c6a
solfany <solfany1999@gmail.com>
2026-07-10 09:42:16 +0900

    Add minimum value validation to ProductCreateRequest price fields

--- src/main/java/com/nplohs/market/product/dto/ProductCreateRequest.java
+++ src/main/java/com/nplohs/market/product/dto/ProductCreateRequest.java
@@ -3,6 +3,7 @@ package com.nplohs.market.product.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nplohs.market.product.entity.ProductCategory;
 import com.nplohs.market.product.entity.ProductType;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -19,6 +20,7 @@ public class ProductCreateRequest {
 
     private String description;
 
+    @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
     private Long price;
 
     @NotNull
@@ -37,8 +39,12 @@ public class ProductCreateRequest {
 
     private List<String> imageIds; // 업로드 후 id 대신 url로 처리하도록 sell 페이지 수정 예정
 
+    @Min(value = 0, message = "경매 시작 가격은 0원 이상이어야 합니다.")
     private Long auctionStartPrice;
+
+    @Min(value = 1, message = "최소 입찰 단위는 1원 이상이어야 합니다.")
     private Long auctionMinBidIncrement;
+
     private String auctionEndAt;
 
     public ProductType getEffectiveType() {
